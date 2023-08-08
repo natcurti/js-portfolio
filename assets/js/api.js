1dfdb01034a6d034bddac2e31bc7aee0292c5aa9
natcurti <128557924+natcurti@users.noreply.github.com>
2023-08-07 22:25:52 -0300

    Alteração Fetch Url

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/natcurti/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/natcurti/js-portfolio/main/data/profile.json';
     const fetching = await fetch(url);
     const profileData = await fetching.json();
     return profileData;
